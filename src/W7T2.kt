25fb004a32b26642e6629182b92daba8d0ed44ff
Azat Mirzayanov <azat.mirzayanov@piano.io>
2020-04-13 20:59:43 +0400

    week 7 task 2

---
+++ src/W7T2.kt
@@ -0,0 +1,84 @@
+import mooc.EdxIO
+
+fun main() {
+    EdxIO.create().use { io ->
+        val n = io.nextInt()
+        val m = hashMapOf<Int, Int>()
+
+        class Node(
+                var key: Int? = null,
+                var h: Int = 0,
+                var parent: Node? = null,
+                var left: Node? = null,
+                var right: Node? = null
+        ) {
+            fun balance() = (right?.h ?: 0) - (left?.h ?: 0)
+        }
+
+        val a = Array<Node?>(n) { null }
+        repeat(n) { i ->
+            val k = io.nextInt()
+            val l = io.nextInt()
+            val r = io.nextInt()
+
+            val node = a[i]?.also { it.key = k } ?: Node(k).also { a[i] = it }
+            if (l != 0) {
+                node.left = Node(parent = node)
+                a[l - 1] = node.left
+            }
+            if (r != 0) {
+                node.right = Node(parent = node)
+                a[r - 1] = node.right
+            }
+        }
+
+        fun h(node: Node?): Int {
+            if (node == null) return 0
+            if (node.h != 0) return node.h
+            val lh = h(node.left)
+            val rh = h(node.right)
+            val h = maxOf(lh, rh)
+            node.h = h + 1
+            return node.h
+        }
+        h(a[0])
+
+        fun rightRotation(root: Node): Node {
+            if (root.left == null) return root
+            var b = root.left!!
+            root.left = b.right
+            b.right = root
+            return b
+        }
+
+        fun leftRotation(root: Node): Node {
+            if (root.right == null) return root
+            var b = root.right!!
+            if (b.balance() == -1) {
+                b = rightRotation(b)
+            }
+            root.right = b.left
+            b.left = root
+            return b
+        }
+
+        val root = leftRotation(a[0]!!)
+        io.println(n)
+        var ai = 0;
+        var bi = 0;
+        a[ai++] = root
+        while (bi < n) {
+            val c = a[bi++]!!
+            val li = if (c.left == null) 0 else {
+                a[ai] = c.left
+                ++ai
+            }
+            val ri = if (c.right == null) 0 else {
+                a[ai] = c.right
+                ++ai
+            }
+            io.println("${c.key} $li $ri")
+        }
+    }
+}
+
